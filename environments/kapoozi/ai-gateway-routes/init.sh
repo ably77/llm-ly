8bf284d4c2fddcc7d8e6283f2254f67c37d2ac6a
Alex <alexbrucely@gmail.com>
2024-07-31 14:53:43 -0700

    filter by ai-gateway-routes project

--- environments/kapoozi/ai-gateway-routes/init.sh
+++ environments/kapoozi/ai-gateway-routes/init.sh
@@ -2,3 +2,21 @@
 
 echo "wave description:"
 echo "deploy ai-gateway routes"
+
+# Create argo app-of-apps project
+kubectl apply --context ${cluster_context} -f- <<EOF
+apiVersion: argoproj.io/v1alpha1
+kind: AppProject
+metadata:
+  name: ai-gateway-routes
+  namespace: argocd
+spec:
+  clusterResourceWhitelist:
+  - group: '*'
+    kind: '*'
+  destinations:
+  - namespace: '*'
+    server: '*'
+  sourceRepos:
+  - '*'
+EOF
